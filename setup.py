79d56dbdd9a8632919071960c52cdf3e2cb924b2
Federico Gonzalez <federicogonzalez761@gmail.com>
2014-06-18 23:15:44 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="couchdblogger",
-    version='0.0.7',
+    version='0.0.8',
     description=read('DESCRIPTION'),
     keywords="couchdb logging logger handler",
     author="Rinat F Sabitov, Federico Gonzalez",
