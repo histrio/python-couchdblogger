3d71467d49c87abc3afe1d4c84780962de0d8529
Ryan Parrish <ryan@stickystyle.net>
2015-08-20 13:57:43 -0400

    use getMessage to properly format messages that have args passed. logger.info("Test %s", "message")

--- src/couchdblogger.py
+++ src/couchdblogger.py
@@ -199,7 +199,7 @@ class CouchDBLogHandler(logging.StreamHandler, object):
                             the record is emitted
         """
         return json.dumps(dict(
-            message=record.msg,
+            message=record.getMessage(),
             level=record.levelname,
             created=record.created,
             logger=record.name
