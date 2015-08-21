7293b32798a7e58e759a055fa0abb4a7f940b465
Rinat Sabitov <rinat.sabitov@gmail.com>
2015-08-21 11:20:22 +0300

    Version up

--- src/couchdblogger.py
+++ src/couchdblogger.py
@@ -10,7 +10,7 @@ import requests
 __author__ = "Rinat F Sabitov, Federico Gonzalez"
 __credits__ = ["Rinat F Sabitov", "Federico Gonzalez"]
 __license__ = "MIT"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __maintainer__ = "Rinat Sabitov"
 __email__ = "rinat.sabitov@gmail.com"
 __status__ = "Development"
